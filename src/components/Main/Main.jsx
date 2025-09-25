9f3568c9ee89c27b32a020ab0ba71cb3197221b5
anggershalahuddin <anggershalahuddin123@gmail.com>
2025-09-25 17:47:37 +0700

    memperbaiki swiper

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,7 +6,7 @@ import Arrow from "../../assets/icon/arrow.svg";
 
 const Main = () => {
   return (
-    <section class="bg-[#E6EAF8] md:grid md:place-content-center">
+    <section class="bg-[#E6EAF8] md:grid md:place-content-center h-full">
       <div class="mx-auto w-full max-w-screen-4xl overflow-hidden">
         <div className="px-12 py-16 gap-5 sm:px-12 sm:py-14 lg:px-16 lg:py-16 flex flex-col items-center justify-center">
           <div className="flex flex-col gap-2 items-center">
@@ -22,6 +22,7 @@ const Main = () => {
               modules={[Autoplay, Pagination]}
               spaceBetween={20}
               slidesPerView={1}
+              anima
               autoplay={{
                 delay: 5000,
                 disableOnInteraction: false,
@@ -36,7 +37,7 @@ const Main = () => {
             >
               {articles.map((article) => (
                 <SwiperSlide key={article.id} className="pb-8 md:pb-12">
-                  <article className="overflow-hidden rounded-lg shadow-sm transition hover:shadow-lg h-full bg-white">
+                  <article className="overflow-hidden rounded-lg shadow-sm transition-transform hover:shadow-lg hover:scale-95 h-full bg-white">
                     <img
                       alt={article.title}
                       src={article.image}
@@ -45,17 +46,17 @@ const Main = () => {
                     <div className="p-4 sm:p-6">
                       <time
                         dateTime={article.date}
-                        className="block text-xs text-gray-500"
+                        className="block text-xs font-medium text-red-700"
                       >
                         {article.date}
                       </time>
                       <a href="/">
-                        <h3 className="mt-0.5 text-lg text-gray-900">
+                        <h3 className="mt-0.5 text-lg text-gray-900 font-semibold">
                           {article.title}
                         </h3>
                       </a>
-                      <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-                        {article.description}
+                      <p className="mt-2 line-clamp-3 text-sm/relaxed">
+                        {article.description.substring(0, 80) + "..."}
                       </p>
                     </div>
                   </article>
