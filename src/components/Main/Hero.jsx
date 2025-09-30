5f4d6730783173eeddf0f7de8eb19436f66bc4f1
anggershalahuddin <anggershalahuddin123@gmail.com>
2025-10-01 00:27:40 +0700

    mengedit tampilah hero dalam mode phone

--- src/components/Main/Hero.jsx
+++ src/components/Main/Hero.jsx
@@ -5,32 +5,39 @@ const Hero = () => {
   return (
     <div className="bg-blue-800">
       <div className="mx-auto flex max-w-screen-4xl h-fit items-center">
-        <div className="grid grid-cols-2 justify-center h-[800px] overflow-hidden text-white">
-          <div className="h-full pl-36 pr-24 xl:pl-42 flex flex-col justify-center gap-8 bg-blue-800">
-            <h1 className="text-4xl font-bold sm:text-5xl">
-              Perawatan Kendaran
-              <strong className="text-orange-400"> Premium </strong>
-               untuk Performa Optimal
-            </h1>
-            <h3 className="font-medium text-base text-pretty sm:text-lg/relaxed">
-              <strong className="text-orange-400">OMPRO AUTOCARE </strong>
-              menyediakan layanan perawatan kendaraan terbaik dengan teknologi
-              mutakhir dan bahan berkualitas tinggi.
-            </h3>
-            <div class="flex gap-4">
-              <a
-                class="inline-block rounded border border-orange-600 bg-orange-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-orange-700 "
-                href="/"
-              >
-                Produk Kami
-              </a>
+        <div className="md:grid md:grid-cols-2 justify-center h-[800px] overflow-hidden text-white">
+          <div className="relative h-full w-full px-12 md:pl-36 xl:pl-42 md:pr-24 flex flex-col justify-center gap-8">
+            <div className="absolute md:hidden inset-0 bg-hero-pattern bg-cover bg-no-repeat bg-center">
+              <div className="absolute inset-0 bg-blue-800/80"></div>
+            </div>
+            <div className="relative z-10 space-y-5">
+              <h1 className="text-4xl font-bold sm:text-5xl text-shadow-lg shadow-black custom-text-shadow">
+                Perawatan Kendaran
+                <strong className="text-orange-400"> Premium </strong>
+                 untuk Performa Optimal
+              </h1>
+              <h3 className="font-medium text-base text-pretty sm:text-lg/relaxed custom-text-shadow">
+                <strong className="text-orange-400 custom-text-shadow">
+                  OMPRO AUTOCARE{" "}
+                </strong>
+                menyediakan layanan perawatan kendaraan terbaik dengan teknologi
+                mutakhir dan bahan berkualitas tinggi.
+              </h3>
+              <div class="flex gap-4">
+                <a
+                  class="inline-block rounded border border-orange-600 bg-orange-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-orange-700 text-sm md:text-base "
+                  href="/"
+                >
+                  Produk Kami
+                </a>
 
-              <a
-                class="inline-block rounded border border-white bg-white px-5 py-3 font-medium text-black shadow-sm transition-colors hover:bg-gray-200 hover:text-gray-900"
-                href="/"
-              >
-                Lihat Artikel
-              </a>
+                <a
+                  class="inline-block rounded border border-white bg-white px-5 py-3 font-medium text-black shadow-sm transition-colors hover:bg-gray-200 hover:text-gray-900 text-sm md:text-base"
+                  href="/"
+                >
+                  Lihat Artikel
+                </a>
+              </div>
             </div>
           </div>
           <img
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
@@ -7,7 +9,28 @@ module.exports = {
         "3xl": "1600px",
         "4xl": "1920px",
       },
+      backgroundImage: {
+        "hero-pattern": "url('/public/img/Ompro-Hero.jpg')",
+      },
+      textShadow: {
+        sm: "0 1px 2px var(--tw-shadow-color)", // Bayangan kecil
+        DEFAULT: "0 2px 4px var(--tw-shadow-color)", // Bayangan default
+        lg: "0 8px 16px var(--tw-shadow-color)", // Bayangan besar
+        none: "none",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          // 'text-shadow-sm', 'text-shadow-DEFAULT', dll.
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") }
+      );
+    }),
+  ],
 };
