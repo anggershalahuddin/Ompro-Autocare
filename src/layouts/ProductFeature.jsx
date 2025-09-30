88be96a5f0278c6a93402fada1c875c9629f9640
anggershalahuddin <anggershalahuddin123@gmail.com>
2025-09-30 23:08:47 +0700

    menambahkan product feature 2

--- src/layouts/ProductFeature.jsx
+++ src/layouts/ProductFeature.jsx
@@ -10,27 +10,27 @@ const ProductFeature = () => {
               <div className="text-3xl md:text-4xl text-center font-bold text-orange-700">
                 Keunggulan Produk Perawatan Kami
               </div>
-              <div className="px-12 md:px-16 text-sm md:text-lg text-center font-semibold">
+              <div className=" md:px-16 text-sm md:text-lg text-center font-semibold text-blue-950">
                 Produk kami terbuat dari bahan berkualitas tinggi yang aman
                 untuk kendaraan Anda. Dengan kemudahan penggunaan, Anda dapat
                 merawat kendaraan Anda tanpa kesulitan. Efek yang tahan lama
                 memastikan kendaraan Anda tetap terlihat baru lebih lama.
               </div>
             </div>
-            <div className="flex flex-col md:flex-row justify-evenly items-center gap-5 px-16 md:px-24 mt-5 border border-black">
-              <div className="w-full h-full shadow-sm transition hover:shadow-lg rounded-lg overflow-hidden">
+            <div className="flex flex-col md:flex-row justify-evenly items-center gap-5 px-8 md:px-24 mt-5">
+              <div className="w-full h-full shadow-md transition hover:shadow-lg rounded-lg overflow-hidden">
                 <img
                   alt=""
                   src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
                 />
                 <div className="bg-white h-full p-4 sm:p-6">
                   <a href="/">
-                    <h3 className="mt-0.5 font-semibold text-lg text-blue-950">
+                    <h3 className="mt-0.5 font-semibold text-md md:text-lg text-blue-950">
                       Bahan Berkualitas Tinggi untuk Perawatan Kendaraan
                     </h3>
                   </a>
 
-                  <p className="mt-2 line-clamp-3 text-sm/relaxed text-black">
+                  <p className="mt-2 line-clamp-3 text-xs md:text-sm/relaxed text-black">
                     Kami menggunakan bahan terbaik untuk hasil maksimal.
                   </p>
                 </div>
@@ -42,12 +42,12 @@ const ProductFeature = () => {
                 />
                 <div className="bg-white h-full p-4 sm:p-6">
                   <a href="/">
-                    <h3 className="mt-0.5 font-semibold text-lg text-blue-950">
+                    <h3 className="mt-0.5 font-semibold text-md md:text-lg text-blue-950">
                       Harga Terjangkau, Ramah di Kantong
                     </h3>
                   </a>
 
-                  <p className="mt-2 line-clamp-3 text-sm/relaxed text-black">
+                  <p className="mt-2 line-clamp-3 text-xs md:text-sm/relaxed text-black">
                     Dengan harga terjangkau kami memberikan kualitas terbaik.
                   </p>
                 </div>
@@ -59,12 +59,12 @@ const ProductFeature = () => {
                 />
                 <div className="bg-white h-full p-4 sm:p-6">
                   <a href="/">
-                    <h3 className="mt-0.5 font-semibold text-lg text-blue-950">
+                    <h3 className="mt-0.5 font-semibold text-md md:text-lg text-blue-950">
                       Jaminan 100% Uang Kembali
                     </h3>
                   </a>
 
-                  <p className="mt-2 line-clamp-3 text-sm/relaxed text-black">
+                  <p className="mt-2 line-clamp-3 text-xs md:text-sm/relaxed text-black">
                     jika produk tidak berfungsi sebagaimana mestinya uang kami
                     kembalikan.
                   </p>
